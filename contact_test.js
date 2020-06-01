0255ebc051e011d53db6baa4ef2a420196289fa3
DineoMabs <dmabitle839@gmail.com>
2020-06-01 18:45:22 +0200

    runnig a test that can fill in a form

--- contact_test.js
+++ contact_test.js
@@ -3,6 +3,16 @@ Feature('contact');
 Scenario('test something', (I) => {
     I.amOnPage('https://www.dotmodus.com');
     I.see('Contact Us');
-
-
+    I.click('Schedule a session');
+    I.fillField('First Name', 'Dineo');
+    I.fillField('Surname', 'Mabitle');
+    I.fillField('Email', 'Dineo@dotmodus.com');
+    I.fillField('Contact Number', '0546874123');
+    I.click('Submit', { css: 'button' });
+    // I.click(locate('.button').withText('Submit'));
+    I.wait(20);
+    //I.click('Submit');
+    After(pause)
+        //I.seeElement({ css: 'id.logo' });
+        // I.see('//button[contains(., "Book a Call")]');
 });
